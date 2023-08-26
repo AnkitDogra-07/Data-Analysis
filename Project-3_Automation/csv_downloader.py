ed58e87d75447643d79e498199433d3f4fc25041
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-26 18:07:59 +0530

    updated csv_downloader.py

--- Project-3_Automation/csv_downloader.py
+++ Project-3_Automation/csv_downloader.py
@@ -1,52 +1,39 @@
-import os 
-import pandas as pd
+import os
 from bs4 import BeautifulSoup
 import requests
-from fake_useragent import UserAgent
-
-ua = UserAgent()
-user_agent = ua.random
+from datetime import datetime  # Import datetime for timestamp
 
 url = "https://www.football-data.co.uk/englandm.php"
-base_url = "https://www.football-data.co.uk"
 
 download_folder = r"C:\Users\ankit\OneDrive\Desktop\Data Analysis\Football-CSV"
 
 if not os.path.exists(download_folder):
     os.makedirs(download_folder)
 
-headers = {"User-Agent": user_agent}
-response = requests.get(url, headers = headers,  verify=True)
+response = requests.get(url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
+
+    links = soup.find_all('a')
     
-    seasons = soup.find_all('h2', string=lambda string: string and string.startswith('Season'))
+    csv_links = [link.get('href') for link in links if link.get('href').endswith('.csv')]
     
-    for season in seasons:
-        season_year = season.text.split(' ')[-1]
-        season_data = season.find_next_sibling('ul')
+    for csv_link in csv_links:
+        full_csv_url = url + csv_link
+        
+        # Generate a unique file name using a timestamp
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        file_name = f"{os.path.basename(csv_link).split('.')[0]}_{timestamp}.csv"
+        
+        file_path = os.path.join(download_folder, file_name)
+        
+        csv_data = requests.get(full_csv_url).content
+        
+        with open(file_path, 'wb') as file:
+            file.write(csv_data)
 
-        links = season_data.find_all('a')
-
-        csv_links = [link.get('href') for link in links if link.get('href').endswith('.csv')]
-
-        for csv_link in csv_links:
-            full_csv_url = base_url + csv_link
-
-            file_name = f"{season_year}_{os.path.basename(csv_link).split('.')[0]}.csv"
-
-            file_path = os.path.join(download_folder, file_name)
-            
-            print(f"Full CSV URL: {full_csv_url}")
-            print(f"File Path: {file_path}")
-
-            csv_data = requests.get(full_csv_url).content
-
-            with open(file_path, 'wb') as file:
-                file.write(csv_data)
-
-            # Print a message indicating that the CSV file has been downloaded
-            print(f"Downloaded CSV file: {file_name}")
+        # Print a message indicating that the CSV file has been downloaded
+        print(f"Downloaded CSV file: {file_name}")
 else:
     print(f"Failed to retrieve the URL. Status code: {response.status_code}")
